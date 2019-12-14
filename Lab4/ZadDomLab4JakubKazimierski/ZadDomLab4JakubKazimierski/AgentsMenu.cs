0f2116aca4ef710185838d00a312aec6a7219620
Jakub <j.m.kazimierski@gmail.com>
2019-12-14 10:33:12 +0100

    List of agents display well

--- Lab4/ZadDomLab4JakubKazimierski/ZadDomLab4JakubKazimierski/AgentsMenu.cs
+++ Lab4/ZadDomLab4JakubKazimierski/ZadDomLab4JakubKazimierski/AgentsMenu.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZadDomLab4JakubKazimierski.Models;
+using ZadDomLab4JakubKazimierski.Repositories;
 
 namespace ZadDomLab4JakubKazimierski
 {
     public partial class AgentsMenu : Form
     {
+        private readonly IFBIgeneric<FBIagentsList> _agents;
+        private readonly IFBIgeneric<FBIagentsType> _agentsType;
         public AgentsMenu()
         {
             InitializeComponent();
+
+            _agents = new FBIgeneric<FBIagentsList>();
+            _agentsType = new FBIgeneric<FBIagentsType>();
+
+            LoadAgents();
+        }
+
+        private void  LoadAgents()
+        {
+            AgentsDataGridView.DataSource = _agents.GetAll();
         }
     }
 }
