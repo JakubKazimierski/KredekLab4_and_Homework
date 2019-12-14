7499ef2c0c3b0d3838a1d49ba1ff9c903c6ea4af
Jakub <j.m.kazimierski@gmail.com>
2019-12-14 06:09:03 +0100

    repaired code from lecture

---
+++ Lab4/kodZajec/EntityFrameworkLab4/Models/Country.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkLab4.Models
+{
+    class Country
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Nazwa Kraju")]
+        public string Name { get; set; }
+
+        [DisplayName("Rekord Kraju")]
+        public float CountryRecord { get; set; }
+        public virtual List<Jumper> Jumpers { get; set; }
+
+    }
+}
