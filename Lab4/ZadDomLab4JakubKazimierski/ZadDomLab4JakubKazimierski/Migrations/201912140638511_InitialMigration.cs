cd8dcb583fd10c84c451f65871e9aa974ed2edfa
Jakub <j.m.kazimierski@gmail.com>
2019-12-14 07:40:26 +0100

    Added Migrations Folder, after using commands in NuGets terminal

---
+++ Lab4/ZadDomLab4JakubKazimierski/ZadDomLab4JakubKazimierski/Migrations/201912140638511_InitialMigration.cs
@@ -0,0 +1,70 @@
+﻿namespace ZadDomLab4JakubKazimierski.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.FBIagentsLists",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false, maxLength: 250),
+                        LastName = c.String(nullable: false),
+                        Birthdate = c.DateTime(nullable: false),
+                        AgentTypeId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.FBIagentsTypes", t => t.AgentTypeId, cascadeDelete: true)
+                .Index(t => t.AgentTypeId);
+            
+            CreateTable(
+                "dbo.FBIagentsTypes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Rank = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.FBIcriminalsLists",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false, maxLength: 250),
+                        LastName = c.String(nullable: false),
+                        Birthdate = c.DateTime(nullable: false),
+                        CriminalTypeId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.FBIcriminalsTypes", t => t.CriminalTypeId, cascadeDelete: true)
+                .Index(t => t.CriminalTypeId);
+            
+            CreateTable(
+                "dbo.FBIcriminalsTypes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Rank = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.FBIcriminalsLists", "CriminalTypeId", "dbo.FBIcriminalsTypes");
+            DropForeignKey("dbo.FBIagentsLists", "AgentTypeId", "dbo.FBIagentsTypes");
+            DropIndex("dbo.FBIcriminalsLists", new[] { "CriminalTypeId" });
+            DropIndex("dbo.FBIagentsLists", new[] { "AgentTypeId" });
+            DropTable("dbo.FBIcriminalsTypes");
+            DropTable("dbo.FBIcriminalsLists");
+            DropTable("dbo.FBIagentsTypes");
+            DropTable("dbo.FBIagentsLists");
+        }
+    }
+}
---
+++ Lab4/ZadDomLab4JakubKazimierski/ZadDomLab4JakubKazimierski/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace ZadDomLab4JakubKazimierski.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ZadDomLab4JakubKazimierski.Models.FBIdbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ZadDomLab4JakubKazimierski.Models.FBIdbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
