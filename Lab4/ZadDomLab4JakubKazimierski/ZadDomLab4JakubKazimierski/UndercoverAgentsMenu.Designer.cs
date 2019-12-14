3b253e9aefb447ea1372cdb4a15463b5a72a1a05
Jakub <j.m.kazimierski@gmail.com>
2019-12-15 00:22:40 +0100

    Synchronization between agents undercover and criminals is working properly now, update insert and delete added to undercover agents

--- Lab4/ZadDomLab4JakubKazimierski/ZadDomLab4JakubKazimierski/UndercoverAgentsMenu.Designer.cs
+++ Lab4/ZadDomLab4JakubKazimierski/ZadDomLab4JakubKazimierski/UndercoverAgentsMenu.Designer.cs
@@ -32,19 +32,15 @@
             this.UpdateGroupBox = new System.Windows.Forms.GroupBox();
             this.UpdateButton = new System.Windows.Forms.Button();
             this.InsertIdLabel = new System.Windows.Forms.Label();
-            this.RankTextBox = new System.Windows.Forms.TextBox();
             this.AgentIdTextBox = new System.Windows.Forms.TextBox();
-            this.RankUpdateLabel = new System.Windows.Forms.Label();
             this.SurnameTextBox = new System.Windows.Forms.TextBox();
             this.AgentUpdateNameLabel = new System.Windows.Forms.Label();
             this.NameTextBox = new System.Windows.Forms.TextBox();
             this.SurnameUpdateLabel = new System.Windows.Forms.Label();
             this.InsertGroupBox = new System.Windows.Forms.GroupBox();
             this.InsertButton = new System.Windows.Forms.Button();
-            this.InsertBirthLabel = new System.Windows.Forms.Label();
-            this.InsertRankTextBox = new System.Windows.Forms.TextBox();
-            this.InsertBirthTextBox = new System.Windows.Forms.TextBox();
-            this.InsertRankLabel = new System.Windows.Forms.Label();
+            this.InsertCriminalIdTextBox = new System.Windows.Forms.TextBox();
+            this.InsertCriminalIdLabel = new System.Windows.Forms.Label();
             this.InsertSurnameTextBox = new System.Windows.Forms.TextBox();
             this.InsertNameLabel = new System.Windows.Forms.Label();
             this.InsertNameTextButton = new System.Windows.Forms.TextBox();
@@ -53,10 +49,12 @@
             this.DeleteButton = new System.Windows.Forms.Button();
             this.DeleteIdTextBox = new System.Windows.Forms.TextBox();
             this.DeleteIDLabel = new System.Windows.Forms.Label();
+            this.CriminalsUndercoverdataGridView = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.UndercoverAgentsDataGridView)).BeginInit();
             this.UpdateGroupBox.SuspendLayout();
             this.InsertGroupBox.SuspendLayout();
             this.DeleteGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CriminalsUndercoverdataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // UndercoverAgentsDataGridView
@@ -72,9 +70,7 @@
             // 
             this.UpdateGroupBox.Controls.Add(this.UpdateButton);
             this.UpdateGroupBox.Controls.Add(this.InsertIdLabel);
-            this.UpdateGroupBox.Controls.Add(this.RankTextBox);
             this.UpdateGroupBox.Controls.Add(this.AgentIdTextBox);
-            this.UpdateGroupBox.Controls.Add(this.RankUpdateLabel);
             this.UpdateGroupBox.Controls.Add(this.SurnameTextBox);
             this.UpdateGroupBox.Controls.Add(this.AgentUpdateNameLabel);
             this.UpdateGroupBox.Controls.Add(this.NameTextBox);
@@ -94,6 +90,7 @@
             this.UpdateButton.TabIndex = 10;
             this.UpdateButton.Text = "Update";
             this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
             // 
             // InsertIdLabel
             // 
@@ -104,13 +101,6 @@
             this.InsertIdLabel.Text = "Insert Agent\'s ID ";
             this.InsertIdLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // RankTextBox
-            // 
-            this.RankTextBox.Location = new System.Drawing.Point(127, 169);
-            this.RankTextBox.Name = "RankTextBox";
-            this.RankTextBox.Size = new System.Drawing.Size(100, 20);
-            this.RankTextBox.TabIndex = 9;
-            // 
             // AgentIdTextBox
             // 
             this.AgentIdTextBox.Location = new System.Drawing.Point(127, 35);
@@ -118,25 +108,16 @@
             this.AgentIdTextBox.Size = new System.Drawing.Size(100, 20);
             this.AgentIdTextBox.TabIndex = 2;
             // 
-            // RankUpdateLabel
-            // 
-            this.RankUpdateLabel.Location = new System.Drawing.Point(15, 169);
-            this.RankUpdateLabel.Name = "RankUpdateLabel";
-            this.RankUpdateLabel.Size = new System.Drawing.Size(100, 23);
-            this.RankUpdateLabel.TabIndex = 6;
-            this.RankUpdateLabel.Text = "Agent Rank";
-            this.RankUpdateLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // SurnameTextBox
             // 
-            this.SurnameTextBox.Location = new System.Drawing.Point(127, 122);
+            this.SurnameTextBox.Location = new System.Drawing.Point(127, 159);
             this.SurnameTextBox.Name = "SurnameTextBox";
             this.SurnameTextBox.Size = new System.Drawing.Size(100, 20);
             this.SurnameTextBox.TabIndex = 8;
             // 
             // AgentUpdateNameLabel
             // 
-            this.AgentUpdateNameLabel.Location = new System.Drawing.Point(6, 78);
+            this.AgentUpdateNameLabel.Location = new System.Drawing.Point(6, 100);
             this.AgentUpdateNameLabel.Name = "AgentUpdateNameLabel";
             this.AgentUpdateNameLabel.Size = new System.Drawing.Size(100, 23);
             this.AgentUpdateNameLabel.TabIndex = 4;
@@ -145,14 +126,14 @@
             // 
             // NameTextBox
             // 
-            this.NameTextBox.Location = new System.Drawing.Point(127, 78);
+            this.NameTextBox.Location = new System.Drawing.Point(127, 100);
             this.NameTextBox.Name = "NameTextBox";
             this.NameTextBox.Size = new System.Drawing.Size(100, 20);
             this.NameTextBox.TabIndex = 7;
             // 
             // SurnameUpdateLabel
             // 
-            this.SurnameUpdateLabel.Location = new System.Drawing.Point(6, 120);
+            this.SurnameUpdateLabel.Location = new System.Drawing.Point(6, 159);
             this.SurnameUpdateLabel.Name = "SurnameUpdateLabel";
             this.SurnameUpdateLabel.Size = new System.Drawing.Size(100, 23);
             this.SurnameUpdateLabel.TabIndex = 5;
@@ -162,10 +143,8 @@
             // InsertGroupBox
             // 
             this.InsertGroupBox.Controls.Add(this.InsertButton);
-            this.InsertGroupBox.Controls.Add(this.InsertBirthLabel);
-            this.InsertGroupBox.Controls.Add(this.InsertRankTextBox);
-            this.InsertGroupBox.Controls.Add(this.InsertBirthTextBox);
-            this.InsertGroupBox.Controls.Add(this.InsertRankLabel);
+            this.InsertGroupBox.Controls.Add(this.InsertCriminalIdTextBox);
+            this.InsertGroupBox.Controls.Add(this.InsertCriminalIdLabel);
             this.InsertGroupBox.Controls.Add(this.InsertSurnameTextBox);
             this.InsertGroupBox.Controls.Add(this.InsertNameLabel);
             this.InsertGroupBox.Controls.Add(this.InsertNameTextButton);
@@ -185,49 +164,34 @@
             this.InsertButton.TabIndex = 10;
             this.InsertButton.Text = "Insert";
             this.InsertButton.UseVisualStyleBackColor = true;
+            this.InsertButton.Click += new System.EventHandler(this.InsertButton_Click);
             // 
-            // InsertBirthLabel
+            // InsertCriminalIdTextBox
             // 
-            this.InsertBirthLabel.Location = new System.Drawing.Point(6, 28);
-            this.InsertBirthLabel.Name = "InsertBirthLabel";
-            this.InsertBirthLabel.Size = new System.Drawing.Size(100, 33);
-            this.InsertBirthLabel.TabIndex = 1;
-            this.InsertBirthLabel.Text = "Birth (YYYY-MM-DD)";
-            this.InsertBirthLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.InsertCriminalIdTextBox.Location = new System.Drawing.Point(127, 169);
+            this.InsertCriminalIdTextBox.Name = "InsertCriminalIdTextBox";
+            this.InsertCriminalIdTextBox.Size = new System.Drawing.Size(100, 20);
+            this.InsertCriminalIdTextBox.TabIndex = 9;
             // 
-            // InsertRankTextBox
+            // InsertCriminalIdLabel
             // 
-            this.InsertRankTextBox.Location = new System.Drawing.Point(127, 169);
-            this.InsertRankTextBox.Name = "InsertRankTextBox";
-            this.InsertRankTextBox.Size = new System.Drawing.Size(100, 20);
-            this.InsertRankTextBox.TabIndex = 9;
-            // 
-            // InsertBirthTextBox
-            // 
-            this.InsertBirthTextBox.Location = new System.Drawing.Point(127, 35);
-            this.InsertBirthTextBox.Name = "InsertBirthTextBox";
-            this.InsertBirthTextBox.Size = new System.Drawing.Size(100, 20);
-            this.InsertBirthTextBox.TabIndex = 2;
-            // 
-            // InsertRankLabel
-            // 
-            this.InsertRankLabel.Location = new System.Drawing.Point(15, 169);
-            this.InsertRankLabel.Name = "InsertRankLabel";
-            this.InsertRankLabel.Size = new System.Drawing.Size(100, 23);
-            this.InsertRankLabel.TabIndex = 6;
-            this.InsertRankLabel.Text = "Agent Rank";
-            this.InsertRankLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.InsertCriminalIdLabel.Location = new System.Drawing.Point(15, 166);
+            this.InsertCriminalIdLabel.Name = "InsertCriminalIdLabel";
+            this.InsertCriminalIdLabel.Size = new System.Drawing.Size(100, 23);
+            this.InsertCriminalIdLabel.TabIndex = 6;
+            this.InsertCriminalIdLabel.Text = "Agent Criminal ID";
+            this.InsertCriminalIdLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // InsertSurnameTextBox
             // 
-            this.InsertSurnameTextBox.Location = new System.Drawing.Point(127, 122);
+            this.InsertSurnameTextBox.Location = new System.Drawing.Point(127, 102);
             this.InsertSurnameTextBox.Name = "InsertSurnameTextBox";
             this.InsertSurnameTextBox.Size = new System.Drawing.Size(100, 20);
             this.InsertSurnameTextBox.TabIndex = 8;
             // 
             // InsertNameLabel
             // 
-            this.InsertNameLabel.Location = new System.Drawing.Point(6, 78);
+            this.InsertNameLabel.Location = new System.Drawing.Point(6, 36);
             this.InsertNameLabel.Name = "InsertNameLabel";
             this.InsertNameLabel.Size = new System.Drawing.Size(100, 23);
             this.InsertNameLabel.TabIndex = 4;
@@ -236,14 +200,14 @@
             // 
             // InsertNameTextButton
             // 
-            this.InsertNameTextButton.Location = new System.Drawing.Point(127, 78);
+            this.InsertNameTextButton.Location = new System.Drawing.Point(127, 39);
             this.InsertNameTextButton.Name = "InsertNameTextButton";
             this.InsertNameTextButton.Size = new System.Drawing.Size(100, 20);
             this.InsertNameTextButton.TabIndex = 7;
             // 
             // InsertSurnameLabel
             // 
-            this.InsertSurnameLabel.Location = new System.Drawing.Point(6, 120);
+            this.InsertSurnameLabel.Location = new System.Drawing.Point(6, 99);
             this.InsertSurnameLabel.Name = "InsertSurnameLabel";
             this.InsertSurnameLabel.Size = new System.Drawing.Size(100, 23);
             this.InsertSurnameLabel.TabIndex = 5;
@@ -270,6 +234,7 @@
             this.DeleteButton.TabIndex = 11;
             this.DeleteButton.Text = "Delete";
             this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
             // 
             // DeleteIdTextBox
             // 
@@ -287,11 +252,21 @@
             this.DeleteIDLabel.Text = "Insert Agent\'s ID ";
             this.DeleteIDLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // CriminalsUndercoverdataGridView
+            // 
+            this.CriminalsUndercoverdataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CriminalsUndercoverdataGridView.Location = new System.Drawing.Point(646, 12);
+            this.CriminalsUndercoverdataGridView.Name = "CriminalsUndercoverdataGridView";
+            this.CriminalsUndercoverdataGridView.RowHeadersWidth = 46;
+            this.CriminalsUndercoverdataGridView.Size = new System.Drawing.Size(327, 597);
+            this.CriminalsUndercoverdataGridView.TabIndex = 14;
+            // 
             // UndercoverAgentsMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(654, 621);
+            this.ClientSize = new System.Drawing.Size(985, 621);
+            this.Controls.Add(this.CriminalsUndercoverdataGridView);
             this.Controls.Add(this.DeleteGroupBox);
             this.Controls.Add(this.InsertGroupBox);
             this.Controls.Add(this.UpdateGroupBox);
@@ -305,6 +280,7 @@
             this.InsertGroupBox.PerformLayout();
             this.DeleteGroupBox.ResumeLayout(false);
             this.DeleteGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CriminalsUndercoverdataGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -315,19 +291,15 @@
         private System.Windows.Forms.GroupBox UpdateGroupBox;
         private System.Windows.Forms.Button UpdateButton;
         private System.Windows.Forms.Label InsertIdLabel;
-        private System.Windows.Forms.TextBox RankTextBox;
         private System.Windows.Forms.TextBox AgentIdTextBox;
-        private System.Windows.Forms.Label RankUpdateLabel;
         private System.Windows.Forms.TextBox SurnameTextBox;
         private System.Windows.Forms.Label AgentUpdateNameLabel;
         private System.Windows.Forms.TextBox NameTextBox;
         private System.Windows.Forms.Label SurnameUpdateLabel;
         private System.Windows.Forms.GroupBox InsertGroupBox;
         private System.Windows.Forms.Button InsertButton;
-        private System.Windows.Forms.Label InsertBirthLabel;
-        private System.Windows.Forms.TextBox InsertRankTextBox;
-        private System.Windows.Forms.TextBox InsertBirthTextBox;
-        private System.Windows.Forms.Label InsertRankLabel;
+        private System.Windows.Forms.TextBox InsertCriminalIdTextBox;
+        private System.Windows.Forms.Label InsertCriminalIdLabel;
         private System.Windows.Forms.TextBox InsertSurnameTextBox;
         private System.Windows.Forms.Label InsertNameLabel;
         private System.Windows.Forms.TextBox InsertNameTextButton;
@@ -336,5 +308,6 @@
         private System.Windows.Forms.Button DeleteButton;
         private System.Windows.Forms.TextBox DeleteIdTextBox;
         private System.Windows.Forms.Label DeleteIDLabel;
+        private System.Windows.Forms.DataGridView CriminalsUndercoverdataGridView;
     }
 }
--- Lab4/ZadDomLab4JakubKazimierski/ZadDomLab4JakubKazimierski/UndercoverAgentsMenu.cs
+++ Lab4/ZadDomLab4JakubKazimierski/ZadDomLab4JakubKazimierski/UndercoverAgentsMenu.cs
@@ -7,14 +7,147 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZadDomLab4JakubKazimierski.Models;
+using ZadDomLab4JakubKazimierski.Repositories;
 
 namespace ZadDomLab4JakubKazimierski
 {
     public partial class UndercoverAgentsMenu : Form
     {
+        private readonly IFBIgeneric<FBIagentsUndercover> _agentsUndercover;
+        private readonly IFBIgeneric<FBIcriminalsList> _criminalsUndercover;
+
+
         public UndercoverAgentsMenu()
         {
             InitializeComponent();
+
+            _agentsUndercover = new FBIgeneric<FBIagentsUndercover>();
+            _criminalsUndercover = new FBIgeneric<FBIcriminalsList>();
+
+            LoadAgents();
+            LoadCriminals();
+        }
+
+        private void LoadCriminals()
+        {
+            CriminalsUndercoverdataGridView.DataSource = _criminalsUndercover.GetAll();
+        }
+        private void LoadAgents()
+        {
+            UndercoverAgentsDataGridView.DataSource = _agentsUndercover.GetAll();
+        }
+
+        private void InsertButton_Click(object sender, EventArgs e)
+        {
+            if (InsertNameTextButton.Text != "" && InsertSurnameTextBox.Text != "" && InsertCriminalIdTextBox.Text != "" )
+            {
+                
+                
+                var agentFirstName = InsertNameTextButton.Text;
+                var agentLastName = InsertSurnameTextBox.Text;
+                var TypeId = InsertCriminalIdTextBox.Text;
+
+                var selectedCriminalIndex = Int32.Parse(InsertCriminalIdTextBox.Text);
+                var findCriminal = _criminalsUndercover.GetById(selectedCriminalIndex);
+
+
+                FBIagentsUndercover newAgent = new FBIagentsUndercover()
+                {
+                    FirstName = agentFirstName,
+                    LastName = agentLastName,
+                    AgentCriminalId = Int32.Parse(TypeId)
+                };
+
+
+                if (findCriminal != null)
+                {
+                    _agentsUndercover.Create(newAgent);
+                    _agentsUndercover.Save();
+
+                    LoadAgents();
+                    LoadCriminals();
+
+                }                                    
+                else
+                {
+                    MessageBox.Show("Criminal ID doesn't exist in data Base. Make sure to input proper data");
+                }
+
+              
+            }
+            else
+            {
+                MessageBox.Show("Insert All Data");
+            }
+
+            InsertNameTextButton.Text = "";
+            InsertSurnameTextBox.Text = "";
+            InsertCriminalIdTextBox.Text = "";
+
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            if (AgentIdTextBox.Text != "" && NameTextBox.Text != "" && SurnameTextBox.Text != "")
+            {
+                var agentId = Int32.Parse(AgentIdTextBox.Text);
+                var updateAgent = _agentsUndercover.GetById(agentId);
+
+                var updateCriminal = _criminalsUndercover.GetById(updateAgent.AgentCriminalId);
+
+                updateCriminal.FirstName = NameTextBox.Text;
+                updateCriminal.LastName = SurnameTextBox.Text;
+
+
+                updateAgent.FirstName = NameTextBox.Text;
+                updateAgent.LastName = SurnameTextBox.Text;
+ 
+
+                _agentsUndercover.Update(updateAgent);
+                _agentsUndercover.Save();
+
+                _criminalsUndercover.Update(updateCriminal);
+                _criminalsUndercover.Save();
+
+                LoadAgents();
+                LoadCriminals();
+            }
+            else
+            {
+                MessageBox.Show("Insert All Data");
+            }
+
+            AgentIdTextBox.Text = "";
+            NameTextBox.Text = "";
+            SurnameTextBox.Text = "";
+
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            if (DeleteIdTextBox.Text != "")
+            {
+                var selectedAgentIndex = Int32.Parse(DeleteIdTextBox.Text);
+                var deleteAgent = _agentsUndercover.GetById(selectedAgentIndex);
+
+
+                var deleteCriminal = _criminalsUndercover.GetById(deleteAgent.AgentCriminalId);
+
+                _criminalsUndercover.DeleteById(deleteCriminal.Id);
+                _criminalsUndercover.Save();
+
+
+
+                LoadAgents();
+                LoadCriminals();
+            }
+            else
+            {
+                MessageBox.Show("Insert ID");
+            }
+
+            DeleteIdTextBox.Text = "";
         }
     }
 }
