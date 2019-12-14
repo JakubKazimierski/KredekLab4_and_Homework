bdae3ba1619bb49cf81efa38eeb8d1e58eaa7a2b
Jakub <j.m.kazimierski@gmail.com>
2019-12-14 07:09:20 +0100

    Added connection between FBIagentsUndercover, and FBIcriminalsList

--- Lab4/ZadDomLab4JakubKazimierski/ZadDomLab4JakubKazimierski/Models/FBIagentsUndercover.cs
+++ Lab4/ZadDomLab4JakubKazimierski/ZadDomLab4JakubKazimierski/Models/FBIagentsUndercover.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,22 @@ namespace ZadDomLab4JakubKazimierski.Models
 {
     class FBIagentsUndercover
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "NVARCHAR")]
+        [StringLength(250)]
+        [DisplayName("Imię")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Pole nazwisko jest wymagane")]
+        [DisplayName("Nazwisko")]
+        public string LastName { get; set; }
+
+        public int AgentCriminalId { get; set; }
+
+        [ForeignKey("AgentCriminalId")]
+        public virtual FBIcriminalsList FBIcriminalsList { get; set; }
     }
 }
